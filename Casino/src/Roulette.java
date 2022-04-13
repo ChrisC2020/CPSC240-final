5d87d4a66b5b1786ec19319f531bc22f37560319
pferris3 <pferris@mail.umw.edu>
2022-04-13 09:42:46 -0400

    Beginning Roulette classes

---
+++ Casino/src/Roulette.java
@@ -0,0 +1,13 @@
+//imports and whatnot
+import java.util.Random;
+
+public class Roulette {
+    //Ball ball = new Ball();
+    //Wheel wheel = new Wheel()
+    Random rng = new Random();
+
+
+    public void spin(){
+
+    }
+}
