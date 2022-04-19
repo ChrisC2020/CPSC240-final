22b1c81215a759c403a911c21d604d563e5c342d
ChrisC2020 <75453255+ChrisC2020@users.noreply.github.com>
2022-04-18 22:12:29 -0400

    Updated Slots & added Images folder. Slots is now fully playable with payouts. Still needs animations added, but the GUI does update to correctly reflect the most recent "play". Please move any images in the Images folder for organization so they don't clutter the class files.

--- Casino/src/Reel.java
+++ Casino/src/Reel.java
@@ -1,20 +1,47 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.concurrent.ThreadLocalRandom;
+
+import static java.lang.Thread.sleep;
+
 public class Reel {
     private int currentFaceInt=0;
     private HashMap<Integer, ReelFaces> faces = new HashMap<Integer, ReelFaces>();
+    private HashMap<Integer, ImageIcon> facePictures = new HashMap<Integer, ImageIcon>();
 
-    public void Populate(){ //puts values into the hashmap so it's not all NULLs
+    public void Populate() throws IOException { //puts values into the hashmaps so they're not all NULLs
+        //faces maps integers to ReelFaces enumerations
+        //facePictures maps integers to pngs for the reel GUI
+        //their ordering matches
         faces.put(0, ReelFaces.barBlue);
+        BufferedImage wPic = ImageIO.read(this.getClass().getResource("Images/0_BAR_BLUE.png"));
+        facePictures.put(0, new ImageIcon(wPic));
         faces.put(1,ReelFaces.sevenWhite);
+        wPic = ImageIO.read(this.getClass().getResource("Images/1_SEVEN_WHITE.png"));
+        facePictures.put(1, new ImageIcon(wPic));
         faces.put(2,ReelFaces.cherry);
+        wPic = ImageIO.read(this.getClass().getResource("Images/2_CHERRY.png"));
+        facePictures.put(2, new ImageIcon(wPic));
         faces.put(3,ReelFaces.barRed);
+        wPic = ImageIO.read(this.getClass().getResource("Images/3_BAR_RED.png"));
+        facePictures.put(3, new ImageIcon(wPic));
         faces.put(4,ReelFaces.sevenBlue);
+        wPic = ImageIO.read(this.getClass().getResource("Images/4_SEVEN_BLUE.png"));
+        facePictures.put(4, new ImageIcon(wPic));
         faces.put(5,ReelFaces.barWhite);
+        wPic = ImageIO.read(this.getClass().getResource("Images/5_BAR_WHITE.png"));
+        facePictures.put(5, new ImageIcon(wPic));
         faces.put(6,ReelFaces.sevenRed);
+        wPic = ImageIO.read(this.getClass().getResource("Images/6_SEVEN_RED.png"));
+        facePictures.put(6, new ImageIcon(wPic));
+        System.out.println("successfully populated");
     }
 
-    public void Spin(){ //advances the currentFace of the Reel a random number of times
+    public void Spin(JLabel foo){ //advances the currentFace of the Reel a random number of times
         int randomNumber = ThreadLocalRandom.current().nextInt(14, 28); //gives a number between 14 & 28
 
         for(; randomNumber>0;randomNumber--){ //counts down the randomNumber, advancing one face at a time till 0
@@ -23,7 +50,71 @@ public class Reel {
             if(currentFaceInt>6){
                 currentFaceInt=0;
             }
+            //put the advance method down here
+            Advance(foo, currentFaceInt);
+            //put something to delay it right here, so there's a moment to observe the "animation"
+            //don't use sleep. Either figure out a better way or leave it as is.
         }
+        //prototype for the advance method.
+        /*
+        try {
+            BufferedImage wPic = ImageIO.read(this.getClass().getResource("leverUpTemp.png"));
+            foo.setIcon(new ImageIcon(wPic));
+        } catch (IOException e) {
+            System.out.println("Reel.Spin() broke.");
+            e.printStackTrace();
+        }*/
+    }
+    public void Advance(JLabel foo, int currentFaceInt) {
+        //consider removing the  try/catch and just handling it elsewhere.
+        //make 1 icon for each state, dependant upon currentFaceInt
+        //hashmap?
+        //change the icon  to be whatever it's supposed to be according to that
+        switch (currentFaceInt){
+            case 0:
+                System.out.println("currentFaceInt = " + currentFaceInt);
+                foo.setIcon(facePictures.get(0));
+
+            case 1:
+                System.out.println("currentFaceInt = " + currentFaceInt);
+                foo.setIcon(facePictures.get(1));
+                break;
+            case 2:
+                System.out.println("currentFaceInt = " + currentFaceInt);
+                foo.setIcon(facePictures.get(2));
+                break;
+            case 3:
+                System.out.println("currentFaceInt = " + currentFaceInt);
+                foo.setIcon(facePictures.get(3));
+                break;
+            case 4:
+                System.out.println("currentFaceInt = " + currentFaceInt);
+                foo.setIcon(facePictures.get(4));
+                break;
+            case 5:
+                System.out.println("currentFaceInt = " + currentFaceInt);
+                foo.setIcon(facePictures.get(5));
+                break;
+            case 6:
+                System.out.println("currentFaceInt = " + currentFaceInt);
+                foo.setIcon(facePictures.get(6));
+                break;
+            default:
+                System.out.println("Something went wrong with the Reel.Advance() method.");
+                break;
+        }
+        //foo.setIcon(facePictures.get(0));
+
+
+
+        /*
+        try {
+            BufferedImage wPic = ImageIO.read(this.getClass().getResource("Images/leverDownTemp.png"));
+            foo.setIcon(new ImageIcon(wPic));
+        } catch (IOException e) {
+            System.out.println("Reel.Advance() broke.");
+            e.printStackTrace();
+        }*/
     }
 
     public ReelFaces getCurrentFace(){ //returns the currentFace
